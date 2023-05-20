2189b35409cbd8b9eb1f7d278cd26e7d479fe6ff
superstar323 <carlosarthur0323@gmail.com>
2023-05-20 03:10:08 -0700

    update

--- src/Router.tsx
+++ src/Router.tsx
@@ -8,10 +8,7 @@ const AppRouter = () => {
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route
-            path="/cert=eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJhY2IxOTBjNDBhY2Y0ZGEzYjM3YTEwNDJmY2EzMWQxMyIsInN1YiI6IntcImFwcFR5cGVcIjpcIjIzNFwiLFwiY2hhbm5lbElkXCI6XCJoNVwiLFwiY3JlYXRlVGltZVwiOjE2ODQzMjQzMjgwMDAsXCJpbnZpdGVDb2RlXCI6XCJBUlFET1dcIixcImlzVGVzdFwiOjAsXCJsYXN0TG9naW5UaW1lXCI6MTY4NDUwMDc3NjA0MCxcIm1lbWJlckxldmVsXCI6MSxcInBhY2thZ2VJZFwiOjQsXCJwYXNzd29yZFwiOlwiYTQxNjcxZDliOTg1NDkwNTExYzdlNDMwNzZlZTIzOWZsb3R0ZXJ5LTIwMjJcIixcInJlZ2lzdGVySXBcIjpcIjIxOC4xOTAuMjQ1LjQ0XCIsXCJyZWdpc3RlclNvdXJjZVwiOlwiaDVcIixcInJlZ2lzdGVyVmVyc2lvbkNvZGVcIjoyMzQsXCJzdGF0dXNcIjowLFwidXBkYXRlVGltZVwiOjE2ODQ1MDA3NzYwNDAsXCJ1c2VySWRcIjoxNjc3MTMsXCJ1c2VyTmFtZVwiOlwiMTExMTFcIixcInVzZXJQaG9uZVwiOlwiMTIzNDU2Nzg4OFwifSIsImlzcyI6InNnIiwiaWF0IjoxNjg0NTAwNzc2fQ.mzk9PbmIqh_ms2bHq1E6w_aECC-592RRGrFCFW7DIkg"
-            element={<Dashboard />}
-          />
+          <Route path="/" element={<Dashboard />} />
           <Route path="/dashboard" element={<Dashboard />} />
         </Routes>
       </Layout>
